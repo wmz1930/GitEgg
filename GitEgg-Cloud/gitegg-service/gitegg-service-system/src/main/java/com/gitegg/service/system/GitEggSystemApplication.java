5be8571fc245b7081cd71fc5595b35aaf9977878
GitEgg <programmer@gitegg.com>
2021-06-23 18:23:38 +0800

    使用Nacos发现、配置和管理微服务

--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/GitEggSystemApplication.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/GitEggSystemApplication.java
@@ -2,11 +2,13 @@ package com.gitegg.service.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * gitegg-system 启动类
  */
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.gitegg")
 @SpringBootApplication
 public class GitEggSystemApplication {
