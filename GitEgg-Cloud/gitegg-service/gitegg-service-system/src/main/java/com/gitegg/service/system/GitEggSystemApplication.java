d181411cf003ffa09125ab81f6c86a64586dbeee
GitEgg <programmer@gitegg.com>
2021-06-23 18:51:43 +0800

    集成Reids缓存

--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/GitEggSystemApplication.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/GitEggSystemApplication.java
@@ -2,6 +2,7 @@ package com.gitegg.service.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @ComponentScan(basePackages = "com.gitegg")
 @SpringBootApplication
+@EnableCaching
 public class GitEggSystemApplication {
 
     public static void main(String[] args) {
--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/controller/SystemController.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/controller/SystemController.java
@@ -7,9 +7,14 @@ import com.gitegg.service.system.service.ISystemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,6 +27,10 @@ import javax.validation.Valid;
 public class SystemController {
 
     private final ISystemService systemService;
+
+    private final RedissonClient redisson;
+
+    private final RedisTemplate<String, String> template;
 
     @Value("${spring.datasource.maxActive}")
     private String nacosMaxActiveType;
@@ -117,6 +126,22 @@ public class SystemController {
     @GetMapping(value = "gateway/forward")
     public Result gatewayForward() {
         return Result.success("gitegg-service-system测试数据");
+    }
+
+    @ApiOperation(value = "缓存测试设置值")
+    @GetMapping(value = "redis/set")
+    public Result redisSet(@RequestParam("id") String id) {
+        RMap<String, String> m = redisson.getMap("test", StringCodec.INSTANCE);
+        m.put("1", id);
+        return Result.success("设置成功");
+    }
+
+    @ApiOperation(value = "缓存测试获取值")
+    @GetMapping(value = "redis/get")
+    public Result redisGet() {
+        BoundHashOperations<String, String, String> hash = template.boundHashOps("test");
+        String t = hash.get("1");
+        return Result.success(t);
     }
 
 }
