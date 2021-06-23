b242843e7acf4999257e80913264da2fbc455890
GitEgg <programmer@gitegg.com>
2021-06-23 18:26:20 +0800

    使用Nacos分布式配置中心

--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/controller/SystemController.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/controller/SystemController.java
@@ -1,23 +1,29 @@
 package com.gitegg.service.system.controller;
 
 import com.gitegg.platform.boot.common.base.Result;
-import com.gitegg.platform.boot.common.exception.BusinessException;
 import com.gitegg.service.system.dto.SystemDTO;
 import com.gitegg.service.system.service.ISystemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "system")
-@AllArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Api(tags = "gitegg-system")
+@RefreshScope
 public class SystemController {
 
     private final ISystemService systemService;
+
+    @Value("${spring.datasource.maxActive}")
+    private String nacosMaxActiveType;
 
     @GetMapping(value = "list")
     @ApiOperation(value = "system list接口")
@@ -42,5 +48,11 @@ public class SystemController {
     @ApiOperation(value = "参数校验测试接口")
     public Result<SystemDTO> valid(@Valid @RequestBody SystemDTO systemDTO) {
         return Result.data(systemDTO);
+    }
+
+    @PostMapping(value = "nacos")
+    @ApiOperation(value = "Nacos读取配置文件测试接口")
+    public Result<String> nacos() {
+        return Result.data(nacosMaxActiveType);
     }
 }
