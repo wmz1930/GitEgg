8779977176457115a1670e8fbe9b5082dfe9f405
GitEgg <programmer@gitegg.com>
2021-06-23 18:10:20 +0800

    集成SpringCloud+SpringBoot

---
+++ GitEgg-Cloud/README.md
@@ -0,0 +1,2 @@
+# GitEgg-Cloud
+SpringCloud企业级应用开发框架GitEgg-Cloud业务开发框架代码
---
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/gitegg/service/system/GitEggSystemApplication.java
@@ -0,0 +1,16 @@
+package com.gitegg.service.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gitegg-system 启动类
+ */
+@SpringBootApplication
+public class GitEggSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitEggSystemApplication.class,args);
+    }
+
+}
