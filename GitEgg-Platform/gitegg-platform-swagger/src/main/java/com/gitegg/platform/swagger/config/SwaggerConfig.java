35e52a0f48f0c94bea236375f4990251921df022
Melon <wmz1930@users.noreply.github.com>
2021-11-15 19:23:01 +0800

    配置优化

--- GitEgg-Platform/gitegg-platform-swagger/src/main/java/com/gitegg/platform/swagger/config/SwaggerConfig.java
+++ GitEgg-Platform/gitegg-platform-swagger/src/main/java/com/gitegg/platform/swagger/config/SwaggerConfig.java
@@ -13,6 +13,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author GitEgg
+ */
 @Configuration
 @EnableSwagger2
 @EnableKnife4j
