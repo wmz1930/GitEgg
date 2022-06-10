890d50951b8b1978ea488f66a91acfce7e99dba7
Melon <wmz1930@users.noreply.github.com>
2022-06-10 10:51:38 +0800

    增加定时任务功能

--- GitEgg-Cloud/gitegg-plugin/gitegg-code-generator/src/main/java/com/gitegg/code/generator/GitEggCodeApplication.java
+++ GitEgg-Cloud/gitegg-plugin/gitegg-code-generator/src/main/java/com/gitegg/code/generator/GitEggCodeApplication.java
@@ -3,6 +3,7 @@ package com.gitegg.code.generator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "com.gitegg")
 @ComponentScan(basePackages = "com.gitegg")
 @SpringBootApplication
+@RefreshScope
 public class GitEggCodeApplication {
 
     public static void main(String[] args) {
---
+++ GitEgg-Cloud/gitegg-plugin/xxl-job-admin/src/main/java/com/xxl/job/admin/core/conf/StaticResourceConfig.java
@@ -0,0 +1,16 @@
+//package com.xxl.job.admin.core.conf;
+//
+//import lombok.AllArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 静态资源配置
+// *
+// */
+//@Configuration
+//@EnableWebMvc
+//@AllArgsConstructor
+//public class StaticResourceConfig implements WebMvcConfigurer {
+//}
